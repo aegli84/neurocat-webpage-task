4519463a64c44af7e0f71d8df664ecbd4aabab86
aegli84 <egli.andreea@yahoo.com>
2021-06-13 22:25:59 +0200

    v2

---
+++ src/components/AboutNeurocat.js
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion'
+// import { pageAnimation } from '../animation'
+// import { titleAnimation, fade, photoAnimation } from '../animation'
+
+
+const About = () => {
+    return(
+        <div>
+            b
+        </div>
+    )
+}
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import ParticlesBackground from '../components/ParticlesBackground'
 import Navbar from '../components/Navbar'
+import About from '../components/AboutNeurocat';
 
 const Home = () => {
     return (
@@ -7,6 +8,7 @@ const Home = () => {
             {/* <div>Test</div> */}
             <Navbar/>
             <ParticlesBackground/>
+            <About/>
         </>
     )
 }
